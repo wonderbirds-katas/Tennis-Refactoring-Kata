6f2f688bdaf922d389abaf9e08c518cdf8a76055
Stefan Boos <kontakt@boos.systems>
2022-11-06 17:57:00 +0000

    feat: hole 5 for TennisGame2.cs
    Squashed:
    - test: remove TennisGame2 constructor parameters to fix failing tests
    - refactor: move method
    - refactor: extract predicate IsWinPlayer2
    - refactor: extract predicate IsWinPlayer1
    - refactor: extract predicate IsAdvantagePlayer2
    - refactor: extract predicate IsAdvantagePlayer1
    - refactor: extract predicate IsDeuce
    - refactor: extract predicate IsTie
    - refactor: rename p1point and p2point to _score1 and _score2, respectively
    - refactor: inline methods P1Score and P2Score
    - refactor: remove unnecessary braces in guard clauses
    - refactor: inline score
    - refactor: inline p1res and p2res
    
    Included the cleanup commit from the beginning of the branch

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -2,52 +2,41 @@ namespace Tennis
 {
     public class TennisGame2 : ITennisGame
     {
-        private int p1point;
-        private int p2point;
+        private int _score1;
+        private int _score2;
 
-        private string p1res = "";
-        private string p2res = "";
-        private string player1Name;
-        private string player2Name;
-
-        public TennisGame2(string player1Name, string player2Name)
+        public void WonPoint(string player)
         {
-            this.player1Name = player1Name;
-            p1point = 0;
-            this.player2Name = player2Name;
+            if (player == "player1")
+                _score1++;
+            else
+                _score2++;
         }
 
         public string GetScore()
         {
-            var score = "";
-            if (p1point == p2point && p1point < 3)
-                return ScoreAsString(p1point) + "-All";
-            if (p1point == p2point && p1point > 2)
-                return "Deuce";
+            if (IsTie()) return ScoreAsString(_score1) + "-All";
+            if (IsDeuce()) return "Deuce";
+            if (IsAdvantagePlayer1()) return "Advantage player1";
+            if (IsAdvantagePlayer2()) return "Advantage player2";
+            if (IsWinPlayer1()) return "Win for player1";
+            if (IsWinPlayer2()) return "Win for player2";
 
-            if (p1point - p2point == 1 && p2point >= 3)
-            {
-                return "Advantage player1";
-            }
-            if (p2point - p1point == 1 && p1point >= 3)
-            {
-                return "Advantage player2";
-            }
-            if (p1point >= 4 && (p1point - p2point) >= 2)
-            {
-                return "Win for player1";
-            }
-            if (p2point >= 4 && (p2point - p1point) >= 2)
-            {
-                return "Win for player2";
-            }
-            
-            p2res = ScoreAsString(p2point);
-            p1res = ScoreAsString(p1point);
-            score = p1res + "-" + p2res;
-            return score;
+            return ScoreAsString(_score1) + "-" + ScoreAsString(_score2);
         }
         
+        private bool IsTie() => _score1 == _score2 && _score1 < 3;
+
+        private bool IsDeuce() => _score1 == _score2 && _score1 > 2;
+        
+        private bool IsAdvantagePlayer1() => _score1 - _score2 == 1 && _score2 >= 3;
+
+        private bool IsAdvantagePlayer2() => _score2 - _score1 == 1 && _score1 >= 3;
+
+        private bool IsWinPlayer1() => _score1 >= 4 && (_score1 - _score2) >= 2;
+
+        private bool IsWinPlayer2() => _score2 >= 4 && (_score2 - _score1) >= 2;
+
         private static string ScoreAsString(int score) =>
             score switch
             {
@@ -57,41 +46,6 @@ namespace Tennis
                 3 => "Forty",
                 _ => ""
             };
-        
-        public void SetP1Score(int number)
-        {
-            for (int i = 0; i < number; i++)
-            {
-                P1Score();
-            }
-        }
-
-        public void SetP2Score(int number)
-        {
-            for (var i = 0; i < number; i++)
-            {
-                P2Score();
-            }
-        }
-
-        private void P1Score()
-        {
-            p1point++;
-        }
-
-        private void P2Score()
-        {
-            p2point++;
-        }
-
-        public void WonPoint(string player)
-        {
-            if (player == "player1")
-                P1Score();
-            else
-                P2Score();
-        }
-
     }
 }
 
