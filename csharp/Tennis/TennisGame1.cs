7153844c0feb97b710dd1665973ebb28166668fb
Stefan Boos <kontakt@boos.systems>
2022-11-06 17:12:28 +0100

    refactor: extract class AdvantagePlayer1

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -42,11 +42,24 @@ namespace Tennis
 
         public Deuce(int points) => _points = points;
 
-        public IGameState AddPointForPlayer1() => new GameState(_points + 1, _points);
+        public IGameState AddPointForPlayer1() => new AdvantagePlayer1(_points + 1);
 
         public IGameState AddPointForPlayer2() => new GameState(_points, _points + 1);
 
         public string AsString() => "Deuce";
+    }
+    
+    internal class AdvantagePlayer1 : IGameState
+    {
+        private readonly int _player1Points;
+
+        public AdvantagePlayer1(int player1Points) => _player1Points = player1Points;
+
+        public IGameState AddPointForPlayer1() => new GameState(_player1Points + 1, _player1Points - 1);
+
+        public IGameState AddPointForPlayer2() => new Deuce(_player1Points);
+        
+        public string AsString() => "Advantage player1";
     }
 
     internal class GameState : IGameState
@@ -69,14 +82,13 @@ namespace Tennis
         {
             if (IsTie(player1Points, player2Points)) return new Tie(player1Points);
             if (IsDeuce(player1Points, player2Points)) return new Deuce(player1Points);
+            if (IsAdvantagePlayer1(player1Points, player2Points)) return new AdvantagePlayer1(player1Points);
 
             return new GameState(player1Points, player2Points);
         }
 
         public string AsString()
         {
-            if (IsAdvantagePlayer1())
-                return "Advantage player1";
             if (IsAdvantagePlayer2())
                 return "Advantage player2";
             if (IsWinPlayer1())
@@ -102,9 +114,9 @@ namespace Tennis
             return (_player1Points >= 4 || _player2Points >= 4) && _player2Points - _player1Points == 1;
         }
 
-        private bool IsAdvantagePlayer1()
+        private bool IsAdvantagePlayer1(int player1Points, int player2Points)
         {
-            return (_player1Points >= 4 || _player2Points >= 4) && _player1Points - _player2Points == 1;
+            return (player1Points >= 4 || player2Points >= 4) && player1Points - player2Points == 1;
         }
 
         private bool IsTie(int player1Points, int player2Points) =>
