5f6359da7b93e8f4746ac9445480155361ba7aff
Stefan Boos <kontakt@boos.systems>
2022-11-06 18:41:19 +0100

    feat: hole 3 for TennisGame2.cs
    Squashed:
    - refactor: replace conditionals with ScoreAsString
    - refactor: replace conditionals with ScoreAsString
    - refactor: combine conditionals
    - refactor: use ScoreAsString instead of assigning "love"
    - refactor: use ScoreAsString instead of assigning "love"

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -25,41 +25,23 @@ namespace Tennis
             if (p1point == p2point && p1point > 2)
                 score = "Deuce";
 
-            if (p1point > 0 && p2point == 0)
+            if ((p1point > 0 && p2point == 0) || (p2point > 0 && p1point == 0))
             {
                 p1res = ScoreAsString(p1point);
-                p2res = "Love";
-                score = p1res + "-Love";
-            }
-            if (p2point > 0 && p1point == 0)
-            {
                 p2res = ScoreAsString(p2point);
-                p1res = "Love";
                 score = p1res + "-" + p2res;
             }
 
             if (p1point > p2point && p1point < 4)
             {
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
+                p1res = ScoreAsString(p1point);
+                p2res = ScoreAsString(p2point);
                 score = p1res + "-" + p2res;
             }
             if (p2point > p1point && p2point < 4)
             {
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
+                p2res = ScoreAsString(p2point);
+                p1res = ScoreAsString(p1point);
                 score = p1res + "-" + p2res;
             }
 
