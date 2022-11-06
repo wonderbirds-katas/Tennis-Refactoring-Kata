428eed7f7cb773c3c26b3352d367b0d9e1d40e95
Stefan Boos <kontakt@boos.systems>
2022-11-06 18:54:00 +0100

    refactor: move conditionals up

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -16,6 +16,23 @@ namespace Tennis
             if (p1point == p2point && p1point > 2)
                 score = "Deuce";
 
+            if (p1point - p2point == 1 && p2point >= 3)
+            {
+                return "Advantage player1";
+            }
+            if (p2point - p1point == 1 && p1point >= 3)
+            {
+                return "Advantage player2";
+            }
+            if (p1point >= 4 && (p1point - p2point) >= 2)
+            {
+                return "Win for player1";
+            }
+            if (p2point >= 4 && (p2point - p1point) >= 2)
+            {
+                return "Win for player2";
+            }
+
             if ((p1point > 0 && p2point == 0) || (p2point > 0 && p1point == 0))
             {
                 p1res = ScoreAsString(p1point);
@@ -29,30 +46,12 @@ namespace Tennis
                 p2res = ScoreAsString(p2point);
                 score = p1res + "-" + p2res;
             }
+
             if (p2point > p1point && p2point < 4)
             {
                 p2res = ScoreAsString(p2point);
                 p1res = ScoreAsString(p1point);
                 score = p1res + "-" + p2res;
-            }
-
-            if (p1point - p2point == 1 && p2point >= 3)
-            {
-                return "Advantage player1";
-            }
-
-            if (p2point - p1point == 1 && p1point >= 3)
-            {
-                return "Advantage player2";
-            }
-
-            if (p1point >= 4 && (p1point - p2point) >= 2)
-            {
-                return "Win for player1";
-            }
-            if (p2point >= 4 && (p2point - p1point) >= 2)
-            {
-                return "Win for player2";
             }
             return score;
         }
