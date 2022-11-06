6475526baed87b4e89f7bb7aef5a9642451b9efa
Stefan Boos <kontakt@boos.systems>
2022-11-06 09:11:18 +0100

    refactor: move fields m_score* to class GameState

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -2,36 +2,37 @@ namespace Tennis
 {
     public class TennisGame1 : ITennisGame
     {
-        private GameState state = new GameState();
-        
-        private int m_score1 = 0;
-        private int m_score2 = 0;
+        private readonly GameState _state = new();
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                m_score1 += 1;
+            {
+                _state.AddPointForPlayer1();
+            }
             else
-                m_score2 += 1;
+            {
+                _state.AddPointForPlayer2();
+            }
         }
 
         public string GetScore()
         {
             if (IsDeuce()) return "Deuce";
-            if (IsTie()) return ScoreAsString(m_score1) + "-" + "All";
+            if (IsTie()) return ScoreAsString(_state.Player1Points) + "-" + "All";
             if (IsAdvantageOrWin()) return AdvantageOrWinnerAsString();
 
-            return ScoreAsString(m_score1) + "-" + ScoreAsString(m_score2);
+            return ScoreAsString(_state.Player1Points) + "-" + ScoreAsString(_state.Player2Points);
         }
 
-        private bool IsAdvantageOrWin() => m_score1 >= 4 || m_score2 >= 4;
+        private bool IsAdvantageOrWin() => _state.Player1Points >= 4 || _state.Player2Points >= 4;
 
-        private bool IsTie() => m_score1 == m_score2;
+        private bool IsTie() => _state.Player1Points == _state.Player2Points;
 
-        private bool IsDeuce() => m_score1 == m_score2 && m_score1 > 2;
+        private bool IsDeuce() => _state.Player1Points == _state.Player2Points && _state.Player1Points > 2;
 
         private string AdvantageOrWinnerAsString() =>
-            (m_score1 - m_score2) switch
+            (_state.Player1Points - _state.Player2Points) switch
             {
                 1 => "Advantage player1",
                 -1 => "Advantage player2",
@@ -52,5 +53,18 @@ namespace Tennis
 
     internal class GameState
     {
+        public int Player1Points { get; private set; }
+
+        public int Player2Points { get; private set; }
+
+        public void AddPointForPlayer1()
+        {
+            Player1Points++;
+        }
+
+        public void AddPointForPlayer2()
+        {
+            Player2Points++;
+        }
     }
 }
