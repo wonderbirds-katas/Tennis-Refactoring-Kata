ecbbebe9194181fba2ceebfb24caa463361dd752
Stefan Boos <kontakt@boos.systems>
2022-11-06 10:03:30 +0100

    refactor: move logic to transform score to string to GameState

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -18,15 +18,47 @@ namespace Tennis
 
         public string GetScore()
         {
-            if (_state.IsDeuce()) return "Deuce";
-            if (_state.IsTie()) return ScoreAsString(_state.Player1Points) + "-" + "All";
-            if (_state.IsAdvantageOrWin()) return AdvantageOrWinnerAsString();
+            return _state.AsString();
+        }
+    }
 
-            return ScoreAsString(_state.Player1Points) + "-" + ScoreAsString(_state.Player2Points);
+    internal class GameState
+    {
+        private int Player1Points { get; init; }
+
+        private int Player2Points { get; init; }
+
+        public GameState AddPointForPlayer1() =>
+            new()
+            {
+                Player1Points = Player1Points + 1,
+                Player2Points = Player2Points
+            };
+
+        public GameState AddPointForPlayer2()=>
+            new()
+            {
+                Player1Points = Player1Points,
+                Player2Points = Player2Points + 1
+            };
+
+        private bool IsAdvantageOrWin() => Player1Points >= 4 || Player2Points >= 4;
+
+        private bool IsTie() => Player1Points == Player2Points;
+
+        private bool IsDeuce() => Player1Points == Player2Points && Player1Points > 2;
+
+        public string AsString()
+        {
+            if (IsDeuce()) return "Deuce";
+            if (IsTie()) return ScoreAsString(Player1Points) + "-" + "All";
+            if (IsAdvantageOrWin()) return AdvantageOrWinnerAsString();
+
+            return ScoreAsString(Player1Points) + "-" + ScoreAsString(Player2Points);
         }
         
         private string AdvantageOrWinnerAsString() =>
-            (_state.Player1Points - _state.Player2Points) switch
+            (Player1Points - Player2Points) switch
             {
                 1 => "Advantage player1",
                 -1 => "Advantage player2",
@@ -43,32 +75,5 @@ namespace Tennis
                 3 => "Forty",
                 _ => ""
             };
-    }
-
-    internal class GameState
-    {
-        public int Player1Points { get; private init; }
-
-        public int Player2Points { get; private init; }
-
-        public GameState AddPointForPlayer1() =>
-            new()
-            {
-                Player1Points = Player1Points + 1,
-                Player2Points = Player2Points
-            };
-
-        public GameState AddPointForPlayer2()=>
-            new()
-            {
-                Player1Points = Player1Points,
-                Player2Points = Player2Points + 1
-            };
-        
-        public bool IsAdvantageOrWin() => Player1Points >= 4 || Player2Points >= 4;
-        
-        public bool IsTie() => Player1Points == Player2Points;
-
-        public bool IsDeuce() => Player1Points == Player2Points && Player1Points > 2;
     }
 }
