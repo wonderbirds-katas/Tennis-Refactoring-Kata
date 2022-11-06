075ece7983481549899c9d7b61252fd6206647f9
Stefan Boos <kontakt@boos.systems>
2022-11-06 19:14:24 +0100

    refactor: rename fields of class

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -2,41 +2,41 @@ namespace Tennis
 {
     public class TennisGame3 : ITennisGame
     {
-        private int p2;
-        private int p1;
-        private string p1N;
-        private string p2N;
+        private int _score2;
+        private int _score1;
+        private string _name1;
+        private string _name2;
 
         public TennisGame3(string player1Name, string player2Name)
         {
-            this.p1N = player1Name;
-            this.p2N = player2Name;
+            this._name1 = player1Name;
+            this._name2 = player2Name;
         }
 
         public string GetScore()
         {
             string s;
-            if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6))
+            if ((_score1 < 4 && _score2 < 4) && (_score1 + _score2 < 6))
             {
                 string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
-                s = p[p1];
-                return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
+                s = p[_score1];
+                return (_score1 == _score2) ? s + "-All" : s + "-" + p[_score2];
             }
             else
             {
-                if (p1 == p2)
+                if (_score1 == _score2)
                     return "Deuce";
-                s = p1 > p2 ? p1N : p2N;
-                return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
+                s = _score1 > _score2 ? _name1 : _name2;
+                return ((_score1 - _score2) * (_score1 - _score2) == 1) ? "Advantage " + s : "Win for " + s;
             }
         }
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                this.p1 += 1;
+                this._score1 += 1;
             else
-                this.p2 += 1;
+                this._score2 += 1;
         }
 
     }
