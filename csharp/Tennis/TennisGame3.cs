5f77d7f5fddb41a89d50a4a74546a459a37f253c
Stefan Boos <kontakt@boos.systems>
2022-11-06 19:33:43 +0100

    refactor: combine declaration and assignment of leadPlayerName

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -19,11 +19,11 @@ namespace Tennis
                 return ScoreAsString(_score1) + "-All";
             if ((_score1 < 4 && _score2 < 4) && (_score1 + _score2 < 6) && (_score1 != _score2))
                 return ScoreAsString(_score1) + "-" + ScoreAsString(_score2);
-
             if (_score1 == _score2)
                 return "Deuce";
-            string leadPlayerName;
-            leadPlayerName = _score1 > _score2 ? _name1 : _name2;
+            
+            var leadPlayerName = _score1 > _score2 ? _name1 : _name2;
+            
             if ((_score1 - _score2) * (_score1 - _score2) == 1)
                 return "Advantage " + leadPlayerName;
             else
