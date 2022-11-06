53973cfbc268f3c32486afaa5a5d2a9e60cd7697
Stefan Boos <kontakt@boos.systems>
2022-11-06 18:55:59 +0100

    refactor: remove obsolete conditionals

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -32,27 +32,10 @@ namespace Tennis
             {
                 return "Win for player2";
             }
-
-            if ((p1point > 0 && p2point == 0) || (p2point > 0 && p1point == 0))
-            {
-                p1res = ScoreAsString(p1point);
-                p2res = ScoreAsString(p2point);
-                score = p1res + "-" + p2res;
-            }
-
-            if (p1point > p2point && p1point < 4)
-            {
-                p1res = ScoreAsString(p1point);
-                p2res = ScoreAsString(p2point);
-                score = p1res + "-" + p2res;
-            }
-
-            if (p2point > p1point && p2point < 4)
-            {
-                p2res = ScoreAsString(p2point);
-                p1res = ScoreAsString(p1point);
-                score = p1res + "-" + p2res;
-            }
+            
+            p2res = ScoreAsString(p2point);
+            p1res = ScoreAsString(p1point);
+            score = p1res + "-" + p2res;
             return score;
         }
 
