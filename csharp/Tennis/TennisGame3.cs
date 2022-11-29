612f6c506006c1028a5bb837fe4d0cc18c9b1e97
Stefan Boos <sboos@igus.net>
2022-11-29 17:13:32 +0100

    refactor: introduce field _scoreStrings

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -6,6 +6,7 @@ namespace Tennis
         private int _score1;
         private readonly string _name1;
         private readonly string _name2;
+        private readonly string[] _scoreStrings = new[] { "Love", "Fifteen", "Thirty", "Forty" };
 
         public TennisGame3(string player1Name, string player2Name)
         {
@@ -26,9 +27,8 @@ namespace Tennis
             string s;
             if ((_score1 < 4 && _score2 < 4) && (_score1 + _score2 < 6))
             {
-                string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
-                s = p[_score1];
-                return (_score1 == _score2) ? s + "-All" : s + "-" + p[_score2];
+                s = _scoreStrings[_score1];
+                return (_score1 == _score2) ? s + "-All" : s + "-" + _scoreStrings[_score2];
             }
             else
             {
