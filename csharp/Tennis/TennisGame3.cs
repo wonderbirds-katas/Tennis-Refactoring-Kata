a056700c9bc329e97362a9beff69c5d0240d7688
Stefan Boos <sboos@igus.net>
2022-11-29 17:32:31 +0100

    refactor(edit): remove outer if

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -23,14 +23,10 @@ namespace Tennis
 
         public string GetScore()
         {
-            if ((_score1 < 4 && _score2 < 4) && (_score1 + _score2 < 6))
-            {
-                if (_score1 < 4 && _score2 < 4 && _score1 + _score2 < 6 && _score1 == _score2)
-                    return ScoreAsString(_score1) + "-All";
-                if (_score1 < 4 && _score2 < 4 && _score1 + _score2 < 6 && _score1 != _score2)
-                    return ScoreAsString(_score1) + "-" + ScoreAsString(_score2);
-            }
-
+            if (_score1 < 4 && _score2 < 4 && _score1 + _score2 < 6 && _score1 == _score2)
+                return ScoreAsString(_score1) + "-All";
+            if (_score1 < 4 && _score2 < 4 && _score1 + _score2 < 6 && _score1 != _score2)
+                return ScoreAsString(_score1) + "-" + ScoreAsString(_score2);
             if (_score1 == _score2)
                 return "Deuce";
             if ((_score1 - _score2) * (_score1 - _score2) == 1)
