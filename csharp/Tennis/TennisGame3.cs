02f0f24d414f66d9116c17eb9f7069e0f939d350
Stefan Boos <sboos@igus.net>
2022-11-29 17:11:28 +0100

    feat: hole 1 for TennisGame3.cs
    refactor:
    - rename vars * 4
    - remove redundant this * 4
    - slide method

--- csharp/Tennis/TennisGame3.cs
+++ csharp/Tennis/TennisGame3.cs
@@ -2,43 +2,41 @@ namespace Tennis
 {
     public class TennisGame3 : ITennisGame
     {
-        private int p2;
-        private int p1;
-        private string p1N;
-        private string p2N;
+        private int _score2;
+        private int _score1;
+        private readonly string _name1;
+        private readonly string _name2;
 
         public TennisGame3(string player1Name, string player2Name)
         {
-            this.p1N = player1Name;
-            this.p2N = player2Name;
-        }
-
-        public string GetScore()
-        {
-            string s;
-            if ((p1 < 4 && p2 < 4) && (p1 + p2 < 6))
-            {
-                string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
-                s = p[p1];
-                return (p1 == p2) ? s + "-All" : s + "-" + p[p2];
-            }
-            else
-            {
-                if (p1 == p2)
-                    return "Deuce";
-                s = p1 > p2 ? p1N : p2N;
-                return ((p1 - p2) * (p1 - p2) == 1) ? "Advantage " + s : "Win for " + s;
-            }
+            _name1 = player1Name;
+            _name2 = player2Name;
         }
 
         public void WonPoint(string playerName)
         {
             if (playerName == "player1")
-                this.p1 += 1;
+                _score1 += 1;
             else
-                this.p2 += 1;
+                _score2 += 1;
         }
 
+        public string GetScore()
+        {
+            string s;
+            if ((_score1 < 4 && _score2 < 4) && (_score1 + _score2 < 6))
+            {
+                string[] p = { "Love", "Fifteen", "Thirty", "Forty" };
+                s = p[_score1];
+                return (_score1 == _score2) ? s + "-All" : s + "-" + p[_score2];
+            }
+            else
+            {
+                if (_score1 == _score2)
+                    return "Deuce";
+                s = _score1 > _score2 ? _name1 : _name2;
+                return ((_score1 - _score2) * (_score1 - _score2) == 1) ? "Advantage " + s : "Win for " + s;
+            }
+        }
     }
 }
-
